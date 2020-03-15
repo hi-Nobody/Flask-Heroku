d0f524fef04e9de9fc7627892d3e35760d9cc645
ming Hi <mingwork683@gmail.com>
2020-03-15 21:01:31 +0800

    Second Deploy

--- app1.py
+++ app1.py
@@ -7,7 +7,7 @@ def home():
 
 @app_test.route("/test")#/test是代表我們要處理的網站路徑
 def test():
-    return "Hello test"
+    return "Hello test123"
 
 if __name__ == "__main__":#如果以主程式執行
     app_test.run(debug=True)#立刻啟動伺服器
